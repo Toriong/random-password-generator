a7c3bcf0e426521112b484202fb8ce4c1646e3dc
gtorion97 <67920345+gtorion97@users.noreply.github.com>
2020-12-09 18:23:50 -0600

    The import file for the random password
    This file contains all of the possible elements that can be randomly chosen as a password for the user.

---
+++ password_character_selection.py
@@ -0,0 +1,14 @@
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+all_caps = alphabet.upper()
+
+special_characters = '~!@#$%^&*_+=-?><|'
+
+numbers = '0123456789'
+
+all_options = alphabet + all_caps + special_characters + numbers
+
+
+
+
